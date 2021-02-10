3a11a08b5da31b475fe9d37a2fa468bc7642d383
Priscila Izuno <priscila.izuno@gmail.com>
2021-02-10 20:16:14 -0300

    novas telas para calculadora

--- calculator-pri/src/app/app.module.ts
+++ calculator-pri/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SomaComponent } from './soma/soma.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SomaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
