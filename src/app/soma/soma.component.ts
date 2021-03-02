0379a59914005b9c4f016cd35be897a929216106
Priscila Izuno <priscila.izuno@gmail.com>
2021-03-01 23:42:25 -0300

    add new features and organization components for calculator-pri

--- src/app/soma/soma.component.ts
+++ src/app/soma/soma.component.ts
@@ -6,15 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./soma.component.sass'],
 })
 export class SomaComponent implements OnInit {
-  numberOne = 20;
-  numberTwo = 10;
-  resultado = 0;
+  numberOne: number;
+  numberTwo: number;
+  resultado: number;
+
+  onInput(event: number){
+    this.numberOne = event.target.value;
+  }
 
   ngOnInit(): void {
     this.soma();
   }
 
   soma(): void {
+    this.numberTwo = 30;
     this.resultado = this.numberOne + this.numberTwo;
   }
 }
